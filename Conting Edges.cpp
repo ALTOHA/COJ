393661f6deddd1fabe9f60b2082de164f128aae7
JEI <toha.developers@gmail.com>
2018-06-25 17:50:04 -0500

    Add files via upload

---
+++ Conting Edges.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+	int casos, val;
+	scanf("%d", &casos);
+	
+	while(casos--){
+		scanf("%d", &val);
+		printf("%d\n", val-1);
+	}	
+	
+	return 0;
+}
