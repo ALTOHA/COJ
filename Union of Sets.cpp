1f15fc60d21c212b2a7ce67692f1c2a61993beda
JEI <toha.developers@gmail.com>
2018-10-03 21:16:54 -0500

    Add files via upload

---
+++ Union of Sets.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	map <int, int> sets;
+	int casos, val, conjuntos;
+	
+	scanf("%d", &casos);
+	
+	while(casos--){
+		scanf("%d", &conjuntos);
+		
+		while(conjuntos--){
+			scanf("%d", &val);
+			
+			sets[val];
+		}
+	}
+	
+	printf("%d\n", sets.size());
+	
+	
+	return 0;
+}
