205b5c555ad8a3015a3ebd3364a42cc21f503d13
JEI <toha.developers@gmail.com>
2018-12-21 11:43:21 -0600

    Add files via upload

---
+++ Exhausting Search.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long int proceso(long long int valor){
+	int temp = valor, cont = 1;
+	
+	if(valor < 10)
+		return valor;
+	
+	else{	
+		while(valor > 0){
+			if(valor < 10)
+				return temp;
+				
+			if(abs((valor%100 / 10) - (valor%10)) != 1){
+				if(cont >= 10){
+					return proceso(temp+cont - (temp%(cont)));
+				}
+				else
+					return proceso(temp+cont);
+			}
+			cont *= 10;
+			valor/=10;
+		}
+	}
+}
+
+int main(){
+	long long int casos, valor, i, temp;
+	bool ac;
+	char res[10], comp[10];
+	scanf("%lld", &casos);
+	
+	while(casos--){
+		ac = false;
+		scanf("%lld", &valor);
+		temp = proceso(valor+1);
+		printf("%lld\n", temp);
+		
+	}
+	
+	return 0;
+}
