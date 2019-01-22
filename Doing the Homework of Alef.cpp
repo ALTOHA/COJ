8118b1e9b3d89fb90ac6d8879631313d61c68f1d
JEI <toha.developers@gmail.com>
2019-01-22 16:01:55 -0600

    Add files via upload

---
+++ Doing the Homework of Alef.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int valor;
+	
+	scanf("%d", &valor);
+
+	printf("625");
+	valor--;
+	
+	while(valor--)
+		printf("0");
+	
+	printf("\n");
+	
+	return 0;
+}
