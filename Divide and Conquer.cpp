e7bee79029979ad8883a0f251dc348b4a8776f79
JEI <toha.developers@gmail.com>
2019-05-23 21:19:24 -0500

    Add files via upload

---
+++ Divide and Conquer.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int numero, div, cont = 0, temp;
+	
+	scanf("%d %d", &numero, &div);
+	
+	printf("%d\n", numero/div);
+
+	return 0;
+}
